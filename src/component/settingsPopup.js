3d4c9212e895d27875b57c222f11557dd2c57933
sajalkhan <sajalhossen@yahoo.com>
2020-12-19 15:03:01 +0600

    fix delete modal position issue

---
+++ src/component/settingsPopup.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function deletePopup({ selectedOption, item }) {
+  return (
+    <div className="-modal">
+      <div>
+        <span>Are you sure?</span>
+      </div>
+      <div className=""></div>
+    </div>
+  );
+}
